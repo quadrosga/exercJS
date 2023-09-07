976465e5808b1222216f16f605e76d45bd4701dd
quadrosga <125706282+quadrosga@users.noreply.github.com>
2023-09-07 16:29:56 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function validaForm() {
+    const numeroPerdas = document.getElementById("numero-perdas").value;
+    const numeroGanhos = document.getElementById("numero-ganhos").value;
+        
+    if (numeroPerdas >= numeroGanhos) {
+        alert("O valor de ganhos deve ser maior que o de perdas!");
+        return false;
+    } else {
+        return true;
+    }
+}
